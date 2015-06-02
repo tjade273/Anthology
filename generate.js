9359995bc032fdedaf51f8f26a57b6bc3144cb96
tjade273 <tjade273@gmail.com>
2015-06-02 16:02:31 -0700

    Working Commit!

--- generate.js
+++ generate.js
@@ -5,8 +5,10 @@ var def = q.defer();
 var dir = path.normalize(process.argv[2]);
 var readdir = q.denodeify(fs.readdir);
 var readFile = q.denodeify(fs.readFile);
+var fileList = []
 function readFiles(files){
   var text = []
+  fileList = files
   files.forEach(function(file){
     text.push(readFile(path.join(dir,file),'utf8'))
   })
@@ -21,11 +23,14 @@ function combine(arr1,arr2){
   }
   return obj
 }
-function concat(arr1,arr2){
-  return arr1+arr2
+function concat(arr1){
+  list = [fileList,arr1]
+  return list
 }
 //REMINDER: call concat after returning q.all(text), then spread
-readdir(dir)
+var obj = readdir(dir)
   .then(readFiles,console.error)
   .then(q.all)
-  .
+  .then(concat)
+  .spread(combine)
+  .then(console.log)
