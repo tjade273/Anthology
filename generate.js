28ddd79806165766cb266aa3f226f57fdf262483
tjade273 <tjade273@gmail.com>
2015-06-02 17:14:44 -0700

    modularized

--- generate.js
+++ generate.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 var path = require('path');
 var q = require('q');
-var def = q.defer();
-var dir = path.normalize(process.argv[2]);
 var readdir = q.denodeify(fs.readdir);
 var readFile = q.denodeify(fs.readFile);
 var fileList = []
@@ -17,7 +15,6 @@ function readFiles(files){
 
 function combine(arr1,arr2){
   var obj = {}
-  console.log("combining...")
   for(i=0;i<arr1.length;i++){
     obj[arr1[i]]=arr2[i];
   }
@@ -27,10 +24,11 @@ function concat(arr1){
   list = [fileList,arr1]
   return list
 }
-//REMINDER: call concat after returning q.all(text), then spread
-var obj = readdir(dir)
+module.exports = function(directory){
+dir = path.normalize(directory);
+return readdir(dir)
   .then(readFiles,console.error)
   .then(q.all)
   .then(concat)
   .spread(combine)
-  .then(console.log)
+}
